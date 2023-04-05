e382eda387fafb372f845b9e294915e7262f39f5
Will Brisbane <brisbane@adobe.com>
2023-04-05 14:07:31 -0400

    Writing code with crayolas

--- blocks/scorecard/scorecard.js
+++ blocks/scorecard/scorecard.js
@@ -1,11 +1,14 @@
-import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
-
 export default async function decorate(block) {
   const link = block.querySelector('a');
   const url = link.href
-  console.log("This is the path " + url);
   const res = await fetch(url);
   const json = await res.json();
-  console.log(json);
-}
 
+  const table = document.createElement('table');
+  const row = document.createElement('tr');
+  const cell = document.createElement('td');
+
+  cell.innerHTML = json;
+  row.append(cell);
+  table.append(row);
+}
